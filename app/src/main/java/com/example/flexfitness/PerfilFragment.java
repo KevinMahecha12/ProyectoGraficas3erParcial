68fe7ea053f43193372b770c35a3d1a50f13ddfd
Rorgrlrio <roger.rs.04.07.02@gmail.com>
2023-06-06 23:05:14 -0600

    Avance perfil

--- app/src/main/java/com/example/flexfitness/PerfilFragment.java
+++ app/src/main/java/com/example/flexfitness/PerfilFragment.java
@@ -1,22 +1,40 @@
 package com.example.flexfitness;
 
+import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.Timestamp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -29,8 +47,16 @@ public class PerfilFragment extends Fragment {
     private int mesAlarma = 0;
     private int anoAlarma = 0;
 
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    String uid = firebaseAuth.getCurrentUser().getUid();
+    DocumentReference docRef = db.collection("usuarios").document(uid);
+
     RadioButton rbMensual, rbAnual;
-    Button btnFecha, btnRegistrarMembresia;
+    Button btnFecha, btnRegistrarMembresia, btnLogout, btnBorrar;
+    LinearLayout nuevaMembresia, membresiaExistente;
+    EditText edtFechaInicio, edtFechaFinal;
+    TextView txtEstatus;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -65,6 +91,7 @@ public class PerfilFragment extends Fragment {
         }
     }
 
+    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -73,8 +100,27 @@ public class PerfilFragment extends Fragment {
 
         btnFecha = root.findViewById(R.id.btnFecha);
         btnRegistrarMembresia = root.findViewById(R.id.btnGuardarMembresia);
+        btnLogout = root.findViewById(R.id.btnLogout);
+        btnBorrar = root.findViewById(R.id.btnBorrarMembresia);
         rbAnual = root.findViewById(R.id.rbAnual);
         rbMensual = root.findViewById(R.id.rbMensual);
+        nuevaMembresia = root.findViewById(R.id.layoutNuevaMembresia);
+        membresiaExistente = root.findViewById(R.id.layoutMembresiaExistente);
+        edtFechaInicio = root.findViewById(R.id.edtFechaInicio);
+        edtFechaFinal = root.findViewById(R.id.edtFechaFin);
+        txtEstatus = root.findViewById(R.id.txtEstatusMembresia);
+
+        nuevaMembresia.setVisibility(View.INVISIBLE);
+        membresiaExistente.setVisibility(View.INVISIBLE);
+
+        existeMembresia();
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logOut();
+            }
+        });
 
         btnFecha.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -90,8 +136,77 @@ public class PerfilFragment extends Fragment {
             }
         });
 
+        btnBorrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                borrarMembresia();
+            }
+        });
 
         return root;
+    }
+
+    public void borrarMembresia(){
+        Map<String, Object> membresiaMap = new HashMap<>();
+
+        docRef.update("membresia", membresiaMap)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        // Actualización exitosa
+                        Toast.makeText(getContext(), "Membresia Borrada Correctamente", Toast.LENGTH_SHORT).show();
+                        existeMembresia();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Error al actualizar
+                        Toast.makeText(getContext(), "PUTAMADE", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }//borrarMembresia
+
+    public void existeMembresia(){
+        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if (documentSnapshot.contains("membresia.fechaInicio")) {
+                    membresiaExistente.setVisibility(View.VISIBLE);
+                    nuevaMembresia.setVisibility(View.INVISIBLE);
+
+                    Log.d("FirestoreData", documentSnapshot.getData().toString());
+
+                    Map<String, Object> membresia = documentSnapshot.getData();
+                    Map<String, Object> fechas = (Map<String, Object>) membresia.get("membresia");
+
+                    Timestamp fechaInicioTimestamp = (Timestamp) fechas.get("fechaInicio");
+                    Timestamp fechaFinalTimestamp = (Timestamp) fechas.get("fechaFinal");
+
+                    // Convertir los timestamps a objetos Date
+                    Date fechaInicio = fechaInicioTimestamp.toDate();
+                    Date fechaFinal = fechaFinalTimestamp.toDate();
+
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+                    String fechaFormateadaInicio = dateFormat.format(fechaInicio);
+                    String fechaFormateadaFinal = dateFormat.format(fechaFinal);
+
+                    edtFechaInicio.setText("Inicio: " + fechaFormateadaInicio);
+                    edtFechaFinal.setText("Final: " + fechaFormateadaFinal);
+                    txtEstatus.setText("Aqui estan los datos registrados de tu membresía:");
+
+                } else {
+                    nuevaMembresia.setVisibility(View.VISIBLE);
+                    membresiaExistente.setVisibility(View.INVISIBLE);
+                    txtEstatus.setText("¡Parece que aún no has registrado tu membresía!");
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getContext(), "Algo salio mal en ExisteMembresia", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public void setFechaMembresia(){
@@ -139,10 +254,14 @@ public class PerfilFragment extends Fragment {
         calendar.set(Calendar.YEAR, anoAlarma);
         calendar.set(Calendar.MONTH, mesAlarma); // Los meses comienzan desde 0 (enero = 0)
         calendar.set(Calendar.DAY_OF_MONTH, diaAlarma);
+        calendar.set(Calendar.HOUR_OF_DAY, 1);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
 
-        Date fecha = calendar.getTime();
+        Date fechaInicio = calendar.getTime();
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
-        String fechaFormateadaInicio = dateFormat.format(fecha);
+        String fechaFormateadaInicio = dateFormat.format(fechaInicio);
 
         //Fecha de fin
         int tempoAno = anoAlarma;
@@ -164,12 +283,40 @@ public class PerfilFragment extends Fragment {
         calendar.set(Calendar.MONTH, tempoMes); // Los meses comienzan desde 0 (enero = 0)
         calendar.set(Calendar.DAY_OF_MONTH, diaAlarma);
 
-        fecha = calendar.getTime();
-        String fechaFormateadaFinal = dateFormat.format(fecha);
+        Date fechaFinal = calendar.getTime();
+        String fechaFormateadaFinal = dateFormat.format(fechaFinal);
 
-        Toast.makeText(getContext(), "Registrado, fecha inicio: " + fechaFormateadaInicio + " fecha final: " + fechaFormateadaFinal, Toast.LENGTH_SHORT).show();
+        //Empieza la firestoreada
+
+        Map<String, Object> membresiaMap = new HashMap<>();
+        membresiaMap.put("fechaFinal", fechaFinal);
+        membresiaMap.put("fechaInicio", fechaInicio);
+        membresiaMap.put("tipo", tipoMembresia);
+
+                docRef.update("membresia", membresiaMap)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        // Actualización exitosa
+                        Toast.makeText(getContext(), "Membresia Registrada Correctamente", Toast.LENGTH_SHORT).show();
+                        existeMembresia();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Error al actualizar
+                        Toast.makeText(getContext(), "PUTAMADE", Toast.LENGTH_SHORT).show();
+                    }
+                });
 
     }//registrarMembresia
+
+    public void logOut(){
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(getContext(), MainActivity.class);
+        startActivity(intent);
+    }
 
 
 }//PerfilFragment
